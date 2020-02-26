3a5935c2cd438cdee16a8609636838b61168e784
munene19 <munenexv@gmail.com>
2020-02-27 02:29:20 +0300

    update admin file

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Neighborhood,Profile,Post,Business
 
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Business)
