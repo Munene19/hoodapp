3d83d96e616d3db99dcb15084aab2496e67fcd1b
munene19 <munenexv@gmail.com>
2020-02-27 11:36:09 +0300

    update forms

--- hood/forms.py
+++ hood/forms.py
@@ -14,11 +14,11 @@ class NeighborhoodForm(forms.ModelForm):
         model = Neighborhood
         fields = [ 'name','location','police','police_department_address','health','health_department_address']
 
-# class BusinessForm(forms.ModelForm):
+class BusinessForm(forms.ModelForm):
     
-#     class Meta:
-#         model = Business
-#         exclude = [ 'business_user','business_neighborhood']
+    class Meta:
+        model = Business
+        exclude = [ 'business_user','business_neighborhood']
 
 # class PostForm(forms.ModelForm):
     
