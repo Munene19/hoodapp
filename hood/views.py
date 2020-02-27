f0f1634ecf6a5377d99192d7fe8069a3f69c92a0
munene19 <munenexv@gmail.com>
2020-02-27 11:31:48 +0300

    signout view updated

--- hood/views.py
+++ hood/views.py
@@ -183,10 +183,10 @@ def search_results(request):
         return render(request, 'search.html',{"message":message})
 
 
-@login_required(login_url='/accounts/login/')
-def leave(request): 
-    current_user = request.user
-    return redirect('home')
+# @login_required(login_url='/accounts/login/')
+# def leave(request): 
+#     current_user = request.user
+#     return redirect('home')
  
         
 # def signout(request):
