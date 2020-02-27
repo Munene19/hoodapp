71385f666447dd23f0f5746de3352e9769dbc31a
munene19 <munenexv@gmail.com>
2020-02-27 11:37:09 +0300

    admin.py added

--- hood/admin.py
+++ hood/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Neighborhood,Profile,Post,Business
 
 # Register your models here.
-# admin.site.register(Neighborhood)
-# admin.site.register(Profile)
-# admin.site.register(Post)
-# admin.site.register(Business)
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Business)
