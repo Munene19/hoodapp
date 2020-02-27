7143fa34a35860399b20e8cbe0bda4654e232046
munene19 <munenexv@gmail.com>
2020-02-27 11:34:18 +0300

    update nighborhood model

--- hood/models.py
+++ hood/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Neighborhood(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100, null=True)
-    posted_by =  models.CharField(max_length=100, null=True)
-    count = models.CharField(max_length=100)
-    police = models.CharField(max_length=100)
-    police_department_address = models.CharField(max_length=100)
-    health = models.CharField(max_length=100)
-    health_department_address = models.CharField(max_length=100)
+# class Neighborhood(models.Model):
+#     name = models.CharField(max_length=100)
+#     location = models.CharField(max_length=100, null=True)
+#     posted_by =  models.CharField(max_length=100, null=True)
+#     count = models.CharField(max_length=100)
+#     police = models.CharField(max_length=100)
+#     police_department_address = models.CharField(max_length=100)
+#     health = models.CharField(max_length=100)
+#     health_department_address = models.CharField(max_length=100)
 
 
     def __str__(self):
